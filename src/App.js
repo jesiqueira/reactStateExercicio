99907be4a9df7d82f9f700f80801ea738b3d14a9
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2023-11-02 09:44:11 -0300

    Exercicio finalizado

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import Produto from './Produto';
-import React from 'react';
+import Produto from "./Produto";
+import React from "react";
 
 function App() {
   const [dados, setDados] = React.useState(null);
+  const [carregando, setCarregando] = React.useState(null);
 
   async function handleClick(event) {
     // console.log(event.target.innerText);
+    setCarregando(true)
     const response = await fetch(
-      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
+      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`
     );
+    console.log(response);
     const json = await response.json();
     // console.log(json);
 
     setDados(json);
+    setCarregando(false)
   }
   return (
     <div>
-      <button style={{ margin: '.5rem' }} onClick={handleClick}>
+      <button style={{ margin: ".5rem" }} onClick={handleClick}>
         Notebook
       </button>
-      <button style={{ margin: '.5rem' }} onClick={handleClick}>
+      <button style={{ margin: ".5rem" }} onClick={handleClick}>
         Smartphone
       </button>
-      <button style={{ margin: '.5rem' }} onClick={handleClick}>
+      <button style={{ margin: ".5rem" }} onClick={handleClick}>
         tablet
       </button>
-      <Produto dados={dados} />
+      {carregando && <p>Carregando...</p>}
+      {!carregando && dados && <Produto dados={dados} />}
     </div>
   );
 }
