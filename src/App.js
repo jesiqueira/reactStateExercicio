4283df69413fc4c853019bd810ed59cc95730404
Jose Edmar de Siqueira <jesiqueira@bbmapfre.com.br>
2023-11-02 08:26:55 -0300

    criado fetch para consultar api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Produto from './Produto';
+import React from 'react';
 
 function App() {
+  const [dados, setDados] = React.useState(null);
+
+  async function handleClick(event) {
+    // console.log(event.target.innerText);
+    const response = await fetch(
+      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
+    );
+    const json = await response.json();
+    // console.log(json);
+
+    setDados(json);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button style={{ margin: '.5rem' }} onClick={handleClick}>
+        Notebook
+      </button>
+      <button style={{ margin: '.5rem' }} onClick={handleClick}>
+        Smartphone
+      </button>
+      <button style={{ margin: '.5rem' }} onClick={handleClick}>
+        tablet
+      </button>
+      <Produto dados={dados} />
     </div>
   );
 }
---
+++ src/Produto.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Produto = ({ dados }) => {
+  return (
+    <div>
+      <h1>{dados.nome}</h1>
+      <h1>{dados.preco}</h1>
+      <h1>{dados.descricao}</h1>
+    </div>
+  );
+};
+
+export default Produto;
